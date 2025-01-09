f3b1ea37099d098c18416c115c0100c5c9c7b975
Riyan Mahmud <118922773+RiyanMahmudbhai@users.noreply.github.com>
2025-01-09 15:58:43 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env
 load_dotenv()
-
-# Telegram Bot API token
 BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
 
 if not BOT_API_TOKEN:
-    raise ValueError("Bot token not found. Please set BOT_API_TOKEN in the .env file.")
+    raise ValueError("Bot token not found. Please set BOT_API_TOKEN.")
