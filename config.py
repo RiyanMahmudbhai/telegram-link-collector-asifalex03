61ab956ca5aee135edb310770a6080aff6d30ea4
Riyan Mahmud <118922773+RiyanMahmudbhai@users.noreply.github.com>
2025-01-09 16:02:09 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
+BOT_API_TOKEN = os.getenv("7771432745:AAFjwPNf-1iwFjyKLb8Z1uJtUpSoQOL87PY")
 
 if not BOT_API_TOKEN:
     raise ValueError("Bot token not found. Please set BOT_API_TOKEN.")
